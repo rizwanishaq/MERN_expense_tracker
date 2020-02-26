dc3e0de73864bceec7412afdee56c020451086b2
rizwanishaq <rizwanishaq@gmail.com>
2020-02-26 09:25:11 +0100

    first commit

---
+++ client/README.md
@@ -0,0 +1,7 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## React Hooks Practice (useContext, and useReducer)
+
+This repo is for understand useContext, and useReducer, and the code is taken from [https://www.youtube.com/watch?v=XuFDcZABiDQ]
+
+#### Special thanks to Brad Traversy for such a good video.
---
+++ client/src/components/TransactionList.js
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
+
+function TransactionList() {
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default TransactionList;
